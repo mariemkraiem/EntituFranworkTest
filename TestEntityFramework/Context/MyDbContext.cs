f42f13962f0b8bc7655b742063d2de5a294404e1
Senraz <meriemkraiem7@gmail.com>
2019-01-21 13:12:16 +0100

    first commit

---
+++ TestEntityFramework/Context/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TestEntityFramework.Models;
+
+namespace TestEntityFramework.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+
+        public MyDbContext() : base()
+        {
+
+        }
+    }
+}
---
+++ TestEntityFramework/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestEntityFramework.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+
+        public virtual List<Project> Projects { get; set; }
+    }
+}
